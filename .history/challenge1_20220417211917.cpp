440d7b5d9b61fbebd76d0903df9dc730242af112
Eidmone Tagaca <etagaca@csub.edu>
2022-04-17 21:20:21 -0700

    added challenge 1

---
+++ .history/challenge1_20220417211917.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  // int num;
+  // cout << "Enter your favorite number.\n";
+  // cin >> num;
+
+  // cout << "Amazing that is my favorite number too! \n";
+  // cout << "No really!!," << num << "is my favorite number too! \n";
+
+  int num1;
+  double num2;
+
+  cout << "Enter num1\n";
+  cin >> num1;
+  cout << "Enter num2\n";
+  cin >> num2;
+
+  cout << "You entered" << num1 << " "  << num2 << endl;
+  
+  return 0; 
+}
